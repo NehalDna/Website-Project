b37ba4ac0c3fdc7f33e4c2dabc9e0b551e454f8b
Nehal Patel <nehalp1988@gmail.com>
2018-12-20 18:33:47 +0000

    Implemented some JavaScript for the nav bar (will make it cleaner soon)

--- js/site.js
+++ js/site.js
@@ -2,6 +2,27 @@ let toggle = document.getElementById('abouttoggle')
 
 
 toggle.addEventListener("click", function(){
-    document.getElementById('modalabout').hidden = false;
+    document.getElementById('modal_about').hidden = false;
+    document.getElementById('modal_project').hidden = true;
+    document.getElementById('modal_contact').hidden = true;
 })
+
+let toggleone = document.getElementById('projecttoggle')
+
+
+toggleone.addEventListener("click", function(){
+    document.getElementById('modal_project').hidden = false;
+    document.getElementById('modal_about').hidden = true;
+    document.getElementById('modal_contact').hidden = true;
+})
+
+let toggletwo = document.getElementById('contacttoggle')
+
+
+toggletwo.addEventListener("click", function(){
+    document.getElementById('modal_contact').hidden = false;
+    document.getElementById('modal_about').hidden = true;
+    document.getElementById('modal_project').hidden = true;
+})
+
 
