9e013ccbc619ba40764d8554b6c3b3b6f12b7d4d
Nehal Patel <nehalp1988@gmail.com>
2018-12-20 23:10:14 +0000

    Removing old code

--- js/site.js
+++ js/site.js
@@ -10,34 +10,3 @@ function show(contentname){
     //shows the specific link content
     document.getElementById(contentname).style.display = "block";
 }
-
-
-
-
-
-
-/*const toggle = document.getElementById('abouttoggle');
-const toggleone = document.getElementById('projecttoggle');
-const toggletwo = document.getElementById('contacttoggle');
-
-
-toggle.addEventListener("click", function(){
-    document.getElementById('modal_about').hidden = false;
-    document.getElementById('modal_project').hidden = true;
-    document.getElementById('modal_contact').hidden = true;
-})
-
-toggleone.addEventListener("click", function(){
-    document.getElementById('modal_project').hidden = false;
-    document.getElementById('modal_about').hidden = true;
-    document.getElementById('modal_contact').hidden = true;
-})
-
-toggletwo.addEventListener("click", function(){
-    document.getElementById('modal_contact').hidden = false;
-    document.getElementById('modal_about').hidden = true;
-    document.getElementById('modal_project').hidden = true;
-})*/
-
-
-
