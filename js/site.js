48ea5c87a7fcc3fbea4f41a096ba6eff94b2439f
Nehal Patel <nehalp1988@gmail.com>
2018-12-20 23:43:42 +0000

    Added a timeout function.

--- js/site.js
+++ js/site.js
@@ -1,12 +1,15 @@
+    
+
 function show(contentname){
     //Hides all the elements with class='content by default */
-
     let i, content, link;
     contents = document.getElementsByClassName('content');
     for(i=0; i<contents.length; i++){
         contents[i].style.display ="none";
     }
-
     //shows the specific link content
     document.getElementById(contentname).style.display = "block";
+    setTimeout(function() {
+        document.getElementById(contentname).style.display = "none";
+      }, 50000);
 }
